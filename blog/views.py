b5cbe1608de1b4fff68033a9cf222a6210852958
jia <jiagarodia18@gmail.com>
2025-03-21 20:51:13 +0530

    gitignore content added

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
