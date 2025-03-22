585db86796a655bf452d9ecf2f0be36f713fe5b3
jia <jiagarodia18@gmail.com>
2025-03-22 15:05:52 +0530

    creation and addition of data

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post1',
-        'content': 'First post content',
-        'date_posted': 'Aug 21, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post2',
-        'content': 'Second post content',
-        'date_posted': 'Aug 28, 2018'
-    },
-]
+from .models import Post
 
 def home(request):
     context ={
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
