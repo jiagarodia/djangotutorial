300e55043b8d5f4a07394764e12f37533259d591
jia <jiagarodia18@gmail.com>
2025-03-23 10:58:31 +0530

    removal of one requirement.txt

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Pofile(models.Model)
+class Pofile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pis')
 
